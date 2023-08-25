32440d7f26f31c5df205b108f8dea81addb9f6fe
seif <seifeddine.aaza@gmail.com>
2023-08-25 11:17:17 +0100

    init

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 
 function getComputerChoice(){
-    let choices = ["rock", "paper", "scisors"]
+  let choices = ["rock", "paper", "scisors"]
   const random = Math.floor(Math.random() * choices.length);
   return choices[random]
 }
-  
+
 function playRound(playerSelection, computerSelection){
-  if(computerSelection == "rock" && playerSelection == "scisors") {
-    return "You lose !"
-  } else if(computerSelection == "rock" && playerSelection == "paper") {
-    return  "You win !"
-  } else if(computerSelection == "paper" && playerSelection == "scisors") {
-    return  "You win !"
-  } else if(computerSelection == "paper" && playerSelection == "rock") {
-    return "You lose !"
-  } else if(computerSelection == "scisors" && playerSelection == "paper") {
-    return "You lose !"
-  } else if(computerSelection == "scisors" && playerSelection == "rock") {
-    return  "You win !"
-  } 
-  else if(computerSelection == playerSelection) {
-    return ( "No winner !")
-  } 
-    else return ("Invalid choice !!")
+  return computerSelection == "rock" && playerSelection == "scisors" ?  "You lose !"
+  : computerSelection == "rock" && playerSelection == "paper" ? "You win !"
+  : computerSelection == "paper" && playerSelection == "scisors" ? "You win !"
+  : computerSelection == "paper" && playerSelection == "rock" ? "You lose !"
+  : computerSelection == "scisors" && playerSelection == "paper" ? "You lose !"
+  : computerSelection == "scisors" && playerSelection == "rock" ? "You win !"
+  : computerSelection == playerSelection ? "No winner !"
+  : "Invalid choice !!"
 }
 
 function game(){
     let score = 0
     for(i=0 ; i<5; i++){
-      if(playRound(prompt("play"),  getComputerChoice()) == "You win !"){
-          score += 1
-        }else score = score
+      playRound(prompt("play"),  getComputerChoice()) == "You win !" ? score += 1 : null
     }
-    if (score > 2) { console.log("You win")} else console.log("You lose") 
-  console.log(score)
+    console.log(score)
+    score > 2 ? console.log("You win !") : console.log("You lose !") 
 }
 
 game()
